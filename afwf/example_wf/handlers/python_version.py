fecdaee0c91e9ffbba6c1147b7205bcbc35a31be
Sanhe <MacHu-GWU@users.noreply.github.com>
2024-01-01 13:04:06 -0500

    update example_wf

--- afwf/example_wf/handlers/python_version.py
+++ afwf/example_wf/handlers/python_version.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from typing import List
+import typing as T
 
 import subprocess
 from pathlib_mate import Path
 
 import attr
 import afwf
-import requests
-from bs4 import BeautifulSoup
 
 from ..cache import cache
 from ... import paths
@@ -17,13 +15,15 @@ from ... import paths
 @attr.define
 class Handler(afwf.Handler):
     @cache.memoize(expire=60)
-    def get_all_python_version(self) -> List[str]:
+    def get_all_python_version(self) -> T.List[str]:
         dir_pyenv = Path(paths.dir_project_root.joinpath("pyenv"))
         dir_pyenv.remove_if_exists()
         with Path(paths.dir_project_root).temp_cwd():
             args = ["git", "clone", "--depth", "1", "https://github.com/pyenv/pyenv"]
             subprocess.run(args, check=True)
-            dir_python_build = paths.dir_project_root.joinpath("pyenv", "plugins", "python-build", "share", "python-build")
+            dir_python_build = paths.dir_project_root.joinpath(
+                "pyenv", "plugins", "python-build", "share", "python-build"
+            )
             versions = list()
             for p in dir_python_build.iterdir():
                 if p.is_file():
@@ -31,15 +31,11 @@ class Handler(afwf.Handler):
                     versions.append(version)
             return versions
 
-    def lower_level_api(self, query: str) -> afwf.ScriptFilter:
+    def main(self, query: str) -> afwf.ScriptFilter:
         if query == "error":
             raise ValueError("query cannot be 'error'!")
         versions = self.get_all_python_version()
-        filtered_versions = [
-            version
-            for version in versions
-            if query in version
-        ]
+        filtered_versions = [version for version in versions if query in version]
         filtered_versions.sort()
         filtered_versions = filtered_versions[:50]
         sf = afwf.ScriptFilter()
@@ -63,8 +59,8 @@ class Handler(afwf.Handler):
             sf.items.append(item)
         return sf
 
-    def handler(self, query: str) -> afwf.ScriptFilter:
-        return self.lower_level_api(query=query)
+    def parse_query(self, query: str) -> T.Dict[str, T.Any]:
+        return dict(query=query)
 
 
 handler = Handler(id="python_version")
