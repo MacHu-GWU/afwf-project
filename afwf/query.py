08c5cb50ed88376794f99670a021ea67dea9bef4
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-31 23:38:59 -0500

    improve query module

--- afwf/query.py
+++ afwf/query.py
@@ -8,24 +8,15 @@ import typing as T
 import attrs
 from attrs_mate import AttrsClass
 
+_SEP = "____"
 
-@attrs.define
-class Query(AttrsClass):
-    """
-    Structured query object.
-
-    :param parts: the parts of query string split by delimiter
-    :param trimmed_parts: similar to parts, but each part is white space stripped
-    """
-    parts: T.List[str] = AttrsClass.ib_list_of_str()
-    trimmed_parts: T.List[str] = AttrsClass.ib_list_of_str()
-
-
-SEP = "____"
 
 class QueryParser:
     """
-    Utility class that can parse string to query.
+    Utility class that can parse string to :class:`Query`. Naturally, it is
+    just a tokenizer.
+
+    :param delimiter: the delimiter to split the query string.
     """
 
     def __init__(
@@ -33,19 +24,78 @@ class QueryParser:
         delimiter: T.Union[str, T.List[str]] = " ",
     ):
         if isinstance(delimiter, str):
-            self.delimiter = [delimiter,]
+            self.delimiter = [delimiter]
         else:
             self.delimiter = delimiter
 
-    def parse(self, s: str) -> Query:
+    def parse(self, s: str) -> "Query":
         """
-        Convert string query to structured query object.
+        Convert string query to structured :class:`Query` object.
+
+        :param s: a string.
         """
+        non_underscore_sep_list = []
         for sep in self.delimiter:
-            s = s.replace(sep, SEP)
-        parts = s.split(SEP)
+            if "_" in sep:
+                s = s.replace(sep, _SEP)
+            else:
+                non_underscore_sep_list.append(sep)
+        for sep in non_underscore_sep_list:
+            s = s.replace(sep, _SEP)
+        parts = s.split(_SEP)
         trimmed_parts = [c.strip() for c in parts if c.strip()]
         return Query(
+            raw=s,
             parts=parts,
             trimmed_parts=trimmed_parts,
         )
+
+
+DEFAULT_QUERY_PARSER = QueryParser()
+
+
+@attrs.define
+class Query(AttrsClass):
+    """
+    Structured query object. This is very useful to parse the input of UI handler.
+
+    :param parts: the parts of query string split by delimiter. For example,
+        if the user input is ``"hello world!"``, then ``parts`` is
+        ``["hello", "world!"]``.
+    :param trimmed_parts: similar to parts, but each part is white-space stripped
+        For example, if the user input is ``" hello world "``, then ``parts`` is
+        ``["", "hello", "world", ""]``, and ``trimmed_parts`` is ``["hello", "world"]``.
+
+    Usage::
+
+        >>> q = Query.from_str("  a   b   c  ")
+        >>> q.trimmed_parts
+        ['a', 'b', 'c']
+        >>> q.n_trimmed_parts
+        3
+    """
+
+    raw: str = AttrsClass.ib_str(nullable=False)
+    parts: T.List[str] = AttrsClass.ib_list_of_str(nullable=False)
+    trimmed_parts: T.List[str] = AttrsClass.ib_list_of_str(nullable=False)
+
+    @classmethod
+    def from_str(cls, s: str, parser=DEFAULT_QUERY_PARSER):
+        """
+        Parse query from string using the given parser.
+        """
+        return parser.parse(s)
+
+    @property
+    def n_parts(self) -> int:
+        """
+        The number of items in the ``parts``.
+        """
+        return len(self.parts)
+
+    @property
+    def n_trimmed_parts(self) -> int:
+        """
+        The number of items in the ``trimmed_parts``.
+        """
+        return len(self.trimmed_parts)
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-from afwf.query import QueryParser
+from afwf.query import Query, QueryParser
 
 
 class TestQueryParser:
@@ -10,14 +9,25 @@ class TestQueryParser:
         q = qp.parse(" bucket / artifacts / / deploy.zip")
         assert q.parts == [" bucket ", " artifacts ", " ", " deploy.zip"]
         assert q.trimmed_parts == ["bucket", "artifacts", "deploy.zip"]
+        assert q.n_parts == 4
+        assert q.n_trimmed_parts == 3
 
         qp = QueryParser(delimiter=[" ", "-", "_"])
         q = qp.parse(" a b-c d_e f-g_h ")
+        assert q.parts == ["", "a", "b", "c", "d", "e", "f", "g", "h", ""]
         assert q.trimmed_parts == list("abcdefgh")
+        assert q.n_parts == 10
+        assert q.n_trimmed_parts == 8
+
+
+class TestQuery:
+    def test_from_str(self):
+        q = Query.from_str("  a   b   c  ")
+        assert q.trimmed_parts == list("abc")
+        assert q.n_trimmed_parts == 3
 
 
 if __name__ == "__main__":
-    import os
+    from afwf.tests import run_cov_test
 
-    basename = os.path.basename(__file__)
-    pytest.main([basename, "-s", "--tb=native"])
+    run_cov_test(__file__, "afwf.query", preview=False)
